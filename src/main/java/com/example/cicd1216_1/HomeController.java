9fc4eb7045aacb74b78b3a7da6df6cc767321480
SMY7527 <kdo7527@naver.com>
2025-12-16 09:43:14 +0900

    first

---
+++ src/main/java/com/example/cicd1216_1/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.cicd1216_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hicicd")
+    public String hicicd() {
+        return "hi, CICD 1216^^";
+    }
+}
